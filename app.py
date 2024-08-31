5144ea13807dd2b63f76853c31d3fe779146c8f3
Eduardo Barra da Veiga Jardim Filho <dudubarra7@gmail.com>
2024-08-31 18:02:24 -0300

    terminado as navbars das paginas

--- app.py
+++ app.py
@@ -5,72 +5,6 @@ import uuid
 from flask_bcrypt import Bcrypt
 from flask import Flask, request, jsonify, redirect, url_for, render_template, session
 
-"""
-app = Flask(__name__)
-app.secret_key = os.urandom(24)
-
-DATABASE = 'Alexandria1.db'
-
-def init_db():
-    with sqlite3.connect(DATABASE) as conn:
-        cursor = conn.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS Clientes (
-            "UUID Cliente" INTEGER PRIMARY KEY AUTOINCREMENT,
-            "Nome Cliente" TEXT,
-            "Email Cliente" TEXT NOT NULL,
-            "Username Cliente" TEXT,
-            "Hash Senha Cliente" TEXT,
-            "Cidade Cliente" TEXT,
-            "Estado Cliente" TEXT,
-            "Pais Cliente" TEXT,
-            "Generos Cliente" TEXT,
-            "Telefone Cliente" TEXT,
-            "Foto Cliente" BLOB
-        )''')
-        conn.commit()
-
-@app.route('/login', methods=['GET'])
-def loginGet():
-    return render_template("login.html")
-
-@app.route('/login', methods=['POST'])
-def loginPost():
-    bcrypt = Bcrypt()
-    dataLogin = request.json
-    email = dataLogin.get('email')
-    password = dataLogin.get('password')
-    with sqlite3.connect(DATABASE) as conn:
-        cursor = conn.cursor()
-
-        # Verifica se o email existe na base
-        cursor.execute('''SELECT "Hash Senha Cliente" FROM Clientes WHERE "Email Cliente" = ?''', (email,))
-        result = cursor.fetchone()
-
-        if not result:
-            return jsonify({'error': 'E-mail não encontrado!'}), 404
-
-        hash_senha_armazenada = result[0]
-
-        # Verifica se o hash da senha inserida é igual ao hash armazenado
-        #hash_senha_inserida = hashlib.sha256(password.encode()).hexdigest()
-        #hash_senha_inserida = hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-        hash_senha_inserida = '$2b$12$EjCFeWMMUtA0HHb5Z.Hi9OAIiMLli.SsjRMrACUkU2idA05Ba0lji'
-        if hash_senha_inserida != hash_senha_armazenada:
-            return jsonify({'error': 'Senha incorreta!'}), 401
-
-        # Redireciona para a página inicial
-        return redirect(url_for('homepage'))
-    
-
-
-@app.route('/', methods=['GET'])
-def homepage():
-    return "Bem-vindo à página inicial!"
-
-if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
-"""
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
@@ -126,7 +60,6 @@ def loginPost():
 
         # Gera um cookie de login (token de login)
         login_token = str(uuid.uuid4())
-        print(login_token)
         # Armazena o token no banco de dados
         cursor.execute('''UPDATE Clientes SET "Login Token" = ? WHERE "UUID Cliente" = ?''', (login_token, uuid_cliente))
         conn.commit()
@@ -141,9 +74,21 @@ def loginPost():
 def homepage():    
     if 'login_token' in session and session['login_token']:
         login_token = session['login_token'] # servir token pra pagina inicial
+        return "Olá mundo!"
     else:
-        return redirect(url_for('loginGet'))
-    
+        return redirect(url_for('loginPost'))
+
+@app.route('/perfil', methods=['GET'])
+def perfilGet():
+    return render_template('perfil.html')
+
+@app.route('/instancia', methods=['GET'])
+def instanciaGet():
+    return render_template('instancia.html')
+
+@app.route('/busca-livros', methods=['GET'])
+def buscaGet():
+    return render_template('busca-livros.html')
 
 if __name__ == '__main__':
     init_db()
